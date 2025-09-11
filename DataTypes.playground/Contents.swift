edf2cdb6b1a8c2336358bb785cad9edb23f64a24
mohamedtamer0 <mt4452242@gmail.com>
2025-09-12 00:19:46 +0300

    update

---
+++ DataTypes.playground/Contents.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+var name:String
+name = "Tamer"
+
+
+var age:Int
+age = 29
+
+//name = 25 //Cannot assign value of type 'Int' to type 'String'
+//age = "Mohamed" //Cannot assign value of type 'String' to type 'Int'
--- Variables-Constants.playground/Contents.swift
+++ Variables-Constants.playground/Contents.swift
@@ -1,3 +1,6 @@
 import UIKit
 
 var name = "Tamer"
+
+let age: Int = 29
+//age = 30 //Cannot assign to value: 'age' is a 'let' constant
