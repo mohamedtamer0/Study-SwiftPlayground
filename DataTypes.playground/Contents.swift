e9eff04a64bc46b48c9fc28de01b838872ef30d7
mohamedtamer0 <mt4452242@gmail.com>
2025-09-12 00:25:53 +0300

    update

--- DataTypes.playground/Contents.swift
+++ DataTypes.playground/Contents.swift
@@ -9,3 +9,19 @@ age = 29
 
 //name = 25 //Cannot assign value of type 'Int' to type 'String'
 //age = "Mohamed" //Cannot assign value of type 'String' to type 'Int'
+
+
+var latitude: Double
+latitude = 31.2342323232323
+
+var longitude: Float
+longitude = -123456.783333333
+
+
+var stayOutTooLate: Bool
+stayOutTooLate = true
+
+var nothingInBrain: Bool
+nothingInBrain = true
+
+var missABeat = false
