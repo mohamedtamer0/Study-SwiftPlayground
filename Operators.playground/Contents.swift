b304bd86c7d35d48ff8d5cd2a6665a3831b29346
mohamedtamer0 <mt4452242@gmail.com>
2025-09-12 00:31:39 +0300

    update

---
+++ Operators.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+
+var a = 10
+a = a + 1
+a = a - 1
+a = a / 1
+a = a * 1
+
+var b = 10
+b += 10
+b -= 10
+
+
+var c = a + b
+
+
+var name1 = "Mohamed"
+var name2 = "Tamer"
+
+var both = name1 + " " + name2
+
+c > 3
+c >= 3
+c < 3
+c <= 3
+
+var nameTest = "Mohamed Tamer"
+nameTest == "Mohamed TamEr"
+
+
+var stayOutTooLate: Bool = true
+stayOutTooLate
+!stayOutTooLate
