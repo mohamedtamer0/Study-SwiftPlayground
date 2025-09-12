e867901c860b81dbf162365d52c04cc53594b4d1
mohamedtamer0 <mt4452242@gmail.com>
2025-09-12 22:06:54 +0300

    update

---
+++ Optionals.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+func getHaterStatus(weather : String ) -> String? {
+    if weather == "Sunny" {
+        return nil
+    }else {
+        return "Hater"
+    }
+}
+
+var status: String?
+status = getHaterStatus(weather: "sunny")
+
+print(status!)
+
+
+
+print("-----------------")
+
+func yearAlbumReleased(name : String) -> Int? {
+    
+    if name == "Taylor Swift" {return 2006}
+    if name == "Test" {return 2008}
+    return nil
+}
+
+var year = yearAlbumReleased(name: "Taylor Swift")
+
+if year == nil {
+    print("There was an error")
+}else {
+    print("It was released in \(year!)")
+}
