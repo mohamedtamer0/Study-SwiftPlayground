4a876cb6d2ce7f4c8663b4533867e53196e31d48
mohamedtamer0 <mt4452242@gmail.com>
2025-09-12 21:01:25 +0300

    update

--- Loops.playground/Contents.swift
+++ Loops.playground/Contents.swift
@@ -27,3 +27,44 @@ var action = ["play", "coach", "manager"]
 for i in 0...2 {
     print("\(people[i]) gonna \(action[i])")
 }
+
+print("---------------")
+
+
+for i in 0..<people.count {
+    var str2 = "\(people[i]) gonna"
+    
+    for _ in 0..<action.count {
+        str2 += " \(action[i])"
+    }
+    
+    print(str2)
+}
+
+
+print("---------------")
+
+
+var count = 0
+
+while true {
+    print("Hello World")
+    count += 1
+    
+    if count == 3 {
+        break
+    }
+}
+
+
+
+print("---------------")
+
+for song in songs {
+    if song == "Hello World" {
+        continue
+    }
+    print("My Fav song is \(song)")
+}
+
+
