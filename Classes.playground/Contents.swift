a23e327125ab50e3e02c85e60a8797106ec221be
mohamedtamer0 <mt4452242@gmail.com>
2025-09-16 04:56:43 +0300

    update

--- Classes.playground/Contents.swift
+++ Classes.playground/Contents.swift
@@ -9,3 +9,47 @@ class Person {
         self.shoes = shoes
     }
 }
+
+
+print("-----------------")
+
+
+@objcMembers class Singer {
+    var name : String
+    var age : Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+    
+    @objc func sing() {
+        print("LA LA LA")
+    }
+}
+
+
+class CountrySinger : Singer {
+    override func sing() {
+        print("Trucks, guitars, and liquor")
+    }
+}
+
+
+class HeavyMetalSinger:Singer {
+    var noiseLevel : Int
+    
+    init(name : String, age: Int, noiseLevel : Int) {
+        self.noiseLevel = noiseLevel
+        super.init(name: name, age: age)
+    }
+    
+    override func sing() {
+        print("Grrrrr rargh rargh rarrrrgh!")
+    }
+}
+
+var tayloer = HeavyMetalSinger(name: "Taylor", age: 30,noiseLevel: 8080)
+tayloer.name
+tayloer.age
+tayloer.sing()
