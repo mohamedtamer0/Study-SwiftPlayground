03864a6a8edf4a48bbaf039f529042a29be24a00
mohamedtamer0 <mt4452242@gmail.com>
2025-11-08 23:07:03 +0200

    Update

--- Classes.playground/Contents.swift
+++ Classes.playground/Contents.swift
@@ -1,3 +1,55 @@
 import UIKit
 
-var greeting = "Hello, playground"
+class Person {
+    var clothes: String
+    var shoes: String
+    
+    init(clothes: String, shoes: String) {
+        self.clothes = clothes
+        self.shoes = shoes
+    }
+}
+
+
+print("-----------------")
+
+
+@objcMembers class Singer {
+    var name : String
+    var age : Int
+    
+    init(name: String, age: Int) {
+        self.name = name
+        self.age = age
+    }
+    
+    @objc func sing() {
+        print("LA LA LA")
+    }
+}
+
+
+class CountrySinger : Singer {
+    override func sing() {
+        print("Trucks, guitars, and liquor")
+    }
+}
+
+
+class HeavyMetalSinger:Singer {
+    var noiseLevel : Int
+    
+    init(name : String, age: Int, noiseLevel : Int) {
+        self.noiseLevel = noiseLevel
+        super.init(name: name, age: age)
+    }
+    
+    override func sing() {
+        print("Grrrrr rargh rargh rarrrrgh!")
+    }
+}
+
+var tayloer = HeavyMetalSinger(name: "Taylor", age: 30,noiseLevel: 8080)
+tayloer.name
+tayloer.age
+tayloer.sing()
