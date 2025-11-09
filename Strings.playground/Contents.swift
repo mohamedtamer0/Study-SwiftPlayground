93114959e7c979f84d546db40b83ddecd288ba6e
mohamedtamer0 <mt4452242@gmail.com>
2025-11-10 00:26:27 +0200

    Update

--- Strings.playground/Contents.swift
+++ Strings.playground/Contents.swift
@@ -4,3 +4,18 @@ var greeting = "Hello, playground"
 
 greeting.count
 greeting.isEmpty
+greeting.contains("s")
+
+// Append and Insert
+greeting.append(contentsOf: ", world!")
+
+
+// Remove and Split
+greeting.removeLast()
+greeting.removeFirst()
+//greeting.removeAll()
+
+
+greeting.split(separator: ",")
+
+print(greeting)
