76f3967b246fa62edd9818373ed0c307c930b760
mohamedtamer0 <mt4452242@gmail.com>
2025-11-10 01:27:24 +0200

    Update

--- Core array methods.playground/Contents.swift
+++ Core array methods.playground/Contents.swift
@@ -1,3 +1,37 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+
+// Chaning & appending items
+var characterClasses: [String] = ["Warrior", "Mage", "Rogue"]
+characterClasses.append("druid")
+characterClasses+=["Paladin", "Sorcerer"]
+
+//Inserting and removing items
+characterClasses.insert("Beas Master",at: 2)
+characterClasses.remove(at: 1)
+
+// Ordering and querying values
+characterClasses.reverse()
+var reversedClasses = characterClasses.reversed()
+
+
+characterClasses.sorted()
+var sortedClasses = characterClasses.sorted()
+
+characterClasses.contains("druid")
+
+
+
+print(characterClasses)
+
+
+
+// 2D arrays and subscripts
+
+var skillTree: [[String]] = [
+    ["Attack", "Defense", "Magic"],
+    ["Strength", "Dexterity", "Intelligence"]
+]
+
+var attackSkill = skillTree[1][1]
--- Swift arrays.playground/Contents.swift
+++ Swift arrays.playground/Contents.swift
@@ -1,3 +1,16 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+
+// Creating arrays
+var levelDifficty: [String] = ["Mohamed", "Hassan", "Ali", "Ahmed", "Oussama"]
+var extraSyntax1 : Array<String> = Array<String>()
+
+// Count and isEmpty
+levelDifficty.count
+levelDifficty.isEmpty
+
+
+// Accessing array values
+var mostDifficult = levelDifficty[1]
+levelDifficty[1] = "Mohab"
