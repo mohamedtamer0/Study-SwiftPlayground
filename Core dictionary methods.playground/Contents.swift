32b6f980b8009cba2c43ecdc4885696b9447eaf3
mohamedtamer0 <mt4452242@gmail.com>
2025-11-10 02:40:24 +0200

    Update

--- Core dictionary methods.playground/Contents.swift
+++ Core dictionary methods.playground/Contents.swift
@@ -1,3 +1,35 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+
+//Caching and overwriting items
+var playerStats: [String: Int] = ["HP":100, "MP":100, "Strength":10]
+var oldValue = playerStats.updateValue(30, forKey: "MP")
+
+
+//playerStats = ["Evsion":12, "HP":100]
+
+
+
+//Caching and removing item
+//playerStats["Strength"] = nil
+var removeValue = playerStats.removeValue(forKey: "Strength")
+
+
+print(playerStats)
+
+
+// Nested dictionaries
+var questBoard = [
+    "Fetch Gemstomes" : [
+        "Objective" : "Find 5 gemstones hidden in the enchanted forest.",
+        "Secret" : "The forest is filled with magical creatures."
+    ],
+    "Defeat the Dragon" : [
+        "Objective" : "Slay the fearsome dragon guarding the treasure.",
+        "Secret" : "The dragon is weak to fire."
+    ]
+]
+
+
+var gemstoneObjective = questBoard["Fetch Gemstomes"]?["Objective"]
--- Swift dictionaries.playground/Contents.swift
+++ Swift dictionaries.playground/Contents.swift
@@ -1,3 +1,18 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+
+// Creating dicionaryies
+var blacksmithShop: [String: Int] = ["Bottle" : 10, "Sword" : 50, "Shield" : 20]
+
+// Accessing and modifying values
+var shieldPrice = blacksmithShop["Shield"]
+blacksmithShop["Bottle"] = 11
+
+blacksmithShop["Towel"] = 1
+print(blacksmithShop)
+
+
+// All keys and values
+let allKeys = [String](blacksmithShop.keys)
+let allValues = [Int](blacksmithShop.values)
