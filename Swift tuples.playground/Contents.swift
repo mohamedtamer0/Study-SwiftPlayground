15b4df684aa510d71bcc351075e1b5cad618cc82
mohamedtamer0 <mt4452242@gmail.com>
2025-11-17 20:26:23 +0200

    Update

--- Swift tuples.playground/Contents.swift
+++ Swift tuples.playground/Contents.swift
@@ -1,3 +1,24 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+// Simple tuple
+var uppercutAttackk : (String,Int, Bool) = ("Uppercut", 10, true)
+uppercutAttackk.0
+uppercutAttackk.1
+uppercutAttackk.2
+
+
+var(attack, damage, isCritical) = uppercutAttackk
+attack
+damage
+isCritical
+
+
+// Naming tuple values
+var planetSmash = (name: "Planet Smash", damage : 200, isCritical: true)
+planetSmash.isCritical
+
+
+// Nameing values with type annitation
+var shieldStomp : (name : String, damage : Int, isCritical : Bool)
+shieldStomp.damage = 100
--- The if statement.playground/Contents.swift
+++ The if statement.playground/Contents.swift
@@ -1,3 +1,19 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+var currebtHealth = 100
+var maxHealth = 100
+
+
+if currebtHealth >= maxHealth {
+    print("HP is at maximum")
+}else if currebtHealth <= 50 {
+    print("Grab some health soon!")
+} else {
+    print("HP is between 50 and 100")
+}
+
+
+if (currebtHealth > 90 || currebtHealth <= maxHealth) {
+    print("You'r doing just fine....")
+}
