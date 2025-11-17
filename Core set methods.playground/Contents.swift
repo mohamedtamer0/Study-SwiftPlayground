adee8f15b537fd30676e08b30fe9b3534666bc24
mohamedtamer0 <mt4452242@gmail.com>
2025-11-17 19:02:33 +0200

    Update

--- Core set methods.playground/Contents.swift
+++ Core set methods.playground/Contents.swift
@@ -1,3 +1,13 @@
 import UIKit
 
-var greeting = "Hello, playground"
+
+// Creating Sets
+var activeQuests: Set = ["Kill the dragon", "Find the golden acorn"]
+var completedQuests: Set = ["Save the princess", "Find the lost treasure", "smite the undead", "Kill the dragon"]
+
+
+// Set operations
+var commonQuests: Set = activeQuests.intersection(completedQuests)
+var differentQuests: Set = activeQuests.symmetricDifference(completedQuests)
+var allQuests: Set = activeQuests.union(completedQuests)
+var ClippedQuests: Set = activeQuests.subtracting(completedQuests)
--- Working with sets.playground/Contents.swift
+++ Working with sets.playground/Contents.swift
@@ -1,3 +1,18 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Creating Sets
+var activeQuests: Set<String> = ["Kill the dragon", "Find the golden acorn"]
+
+
+// Inserting and removing elements
+activeQuests.insert("Collect shiny rocks")
+activeQuests.remove("Kill the dragon")
+
+
+
+
+print(activeQuests)
+
+// More Common Methods
+activeQuests.contains("Test")
+activeQuests.sorted()
