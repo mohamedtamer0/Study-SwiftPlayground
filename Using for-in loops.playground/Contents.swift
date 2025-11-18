bd0f8b528116d9c912b6ce0cd7dd75b1bb059d28
mohamedtamer0 <mt4452242@gmail.com>
2025-11-19 00:37:25 +0200

    update

--- Using for-in loops.playground/Contents.swift
+++ Using for-in loops.playground/Contents.swift
@@ -1,3 +1,19 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Test Variables
+let playerGreeting = "Hello fellow Hunter!"
+let armorTypes = ["Heavy Armor", "Light Armor", "Shield"]
+let weapons = ["longsword": 10, "shortsword": 5, "bow": 20]
+
+// Strings and arrays
+for stringCharacter in playerGreeting {
+    print(stringCharacter)
+}
+
+for armorType in armorTypes {
+    print(armorType)
+}
+
+for weaponValues in weapons.values {
+    print(weaponValues)
+}
